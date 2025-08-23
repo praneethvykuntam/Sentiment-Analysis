c8a36fef60e5894a43fb66e09273a7ced893e808
praneethvykuntam <praneethvykuntam2001@gmail.com>
2025-08-23 00:12:40 -0400

    chore: initialize project using cookiecutter-data-science template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ReviewPulse helps companies make sense of thousands of customer reviews by automatically classifying them as positive or negative and surfacing the real reasons behind complaints. Instead of guessing, managers can see common themes like “late delivery” or “poor battery life”, understand the trade-offs (e.g., cheaper products drive sales but generate more negative feedback), and act on data-driven insights to improve products, service, and profitability.',
+    author='PraneethVykuntam',
+    license='MIT',
+)
